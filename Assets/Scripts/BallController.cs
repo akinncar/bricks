c4e7547d95575ec8e11f93e372dc3ba7165996df
Akinn Rosa <akinncar@hotmail.com>
2022-02-16 22:31:28 -0300

    feat: rushing mvp

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,55 +4,55 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    private BallModel _ballModel;
-    private Rigidbody2D _ballRigibody;
+  private BallModel _ballModel;
+  private Rigidbody2D _ballRigibody;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _ballModel = GetComponent<BallModel>();
-        _ballRigibody = GetComponent<Rigidbody2D>();
+  // Start is called before the first frame update
+  void Start()
+  {
+    _ballModel = GetComponent<BallModel>();
+    _ballRigibody = GetComponent<Rigidbody2D>();
 
-        _ballRigibody.velocity = _ballModel.Direction * _ballModel.Speed;
-    }
+    _ballRigibody.velocity = _ballModel.Direction * _ballModel.Speed;
+  }
 
-    // Update is called once per frame
-    public void PerfectAngleReflect(Collision2D collision)
-    {
-        Vector2 direction = Vector2.Reflect(_ballModel.Direction, collision.contacts[0].normal);
-        AngleChange(direction);
-    }
+  // Update is called once per frame
+  public void PerfectAngleReflect(Collision2D collision)
+  {
+    Vector2 direction = Vector2.Reflect(_ballModel.Direction, collision.contacts[0].normal);
+    AngleChange(direction);
+  }
 
-    public void CalcBallAngleReflect(Collision2D colision)
-    {
-        // calc angle for player reflect
-        float playerPixels = 250f * 0.4f;
+  public void CalcBallAngleReflect(Collision2D colision)
+  {
+    // calc angle for player reflect
+    float playerPixels = 250f * 0.4f;
 
-        // convert sizemto unity scale
-        float unityScaleHalfPlayerPixels = playerPixels / 2f / 100f;
+    // convert sizemto unity scale
+    float unityScaleHalfPlayerPixels = playerPixels / 2f / 100f;
 
-        // convert 0 to 120 (player) to 0 to 180 (angle)
-        float scaleFactorPut1dot180Range = 180 / playerPixels;
+    // convert 0 to 120 (player) to 0 to 180 (angle)
+    float scaleFactorPut1dot180Range = 180 / playerPixels;
 
-        // calc angle from unity scale (0 > 1.8)
-        float angleDegUnityScale = (
-            colision.transform.position.x -
-            transform.position.x +
-            unityScaleHalfPlayerPixels
-        ) * scaleFactorPut1dot180Range;
+    // calc angle from unity scale (0 > 1.8)
+    float angleDegUnityScale = (
+        colision.transform.position.x -
+        transform.position.x +
+        unityScaleHalfPlayerPixels
+    ) * scaleFactorPut1dot180Range;
 
-        float angleDeg = angleDegUnityScale * 100f;
+    float angleDeg = angleDegUnityScale * 100f;
 
-        float angleRad = angleDeg * Mathf.PI / 180f;
+    float angleRad = angleDeg * Mathf.PI / 180f;
 
-        Vector2 vetorRetorno = new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    Vector2 vetorRetorno = new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
 
-        AngleChange(vetorRetorno);
-    }
+    AngleChange(vetorRetorno);
+  }
 
-    public void AngleChange(Vector2 direcion)
-    {
-        _ballModel.Direction = direcion;
-        _ballRigibody.velocity = _ballModel.Direction * _ballModel.Speed;
-    }
+  public void AngleChange(Vector2 direcion)
+  {
+    _ballModel.Direction = direcion;
+    _ballRigibody.velocity = _ballModel.Direction * _ballModel.Speed;
+  }
 }
--- Assets/Scripts/BallView.cs
+++ Assets/Scripts/BallView.cs
@@ -5,51 +5,60 @@ using UnityEngine;
 
 public class BallView : MonoBehaviour
 {
-    private BallController _ballController;
-    Vector3 myVector;
-    Text life;
+  private BallController _ballController;
+  Vector3 myVector;
+  Text life;
+  AudioSource _sourceAudio;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    _ballController = GetComponent<BallController>();
+    _sourceAudio = GetComponent<AudioSource>();
+  }
+
+  // Update is called once per frame
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    // verify game object type to take damage
+    if (collision.gameObject.tag == "enemy")
     {
-        _ballController = GetComponent<BallController>();
+      _sourceAudio.Play();
+      BrickView _brickView = collision.gameObject.GetComponent<BrickView>();
+      _brickView.PerformTakeDamage(1f);
+    }
+    else if (collision.gameObject.tag == "Finish")
+    {
+      int oldLife;
+      int newLife;
+
+      life = GameObject.Find("Life").GetComponent<Text>();
+
+      if (life.text != "0")
+      {
+        int.TryParse(life.text, out oldLife);
+        newLife = oldLife - 1;
+
+        life.text = newLife.ToString();
+
+        myVector = new Vector3(-1.32f, -3.71f, 0.0f);
+        _ballController.transform.SetPositionAndRotation(myVector, new Quaternion());
+      };
+
     }
 
-    // Update is called once per frame
-    private void OnCollisionEnter2D(Collision2D collision)
+    // moviment ball change when player crash
+    if (collision.gameObject.tag == "Player")
     {
-        // verify game object type to take damage
-        if (collision.gameObject.tag == "enemy")
-        {
-            BrickView _brickView = collision.gameObject.GetComponent<BrickView>();
-            _brickView.PerformTakeDamage(1f);
-        }
-        else if (collision.gameObject.tag == "Finish")
-        {
-            int oldLife;
-            int newLife;
-
-            life = GameObject.Find("Life").GetComponent<Text>();
-            int.TryParse(life.text, out oldLife);
-            newLife = oldLife - 1;
-
-            life.text = newLife.ToString();
-
-            myVector = new Vector3(-1.32f, -3.71f, 0.0f);
-            _ballController.transform.SetPositionAndRotation(myVector, new Quaternion());
-        }
-
-        // moviment ball change when player crash
-        if (collision.gameObject.tag == "Player")
-        {
-            _ballController.CalcBallAngleReflect(collision);
-        }
-        else
-        {
-            // loads normal angle
-            _ballController.PerfectAngleReflect(collision);
-        }
-
-        
+      _sourceAudio.Play();
+      _ballController.CalcBallAngleReflect(collision);
     }
+    else
+    {
+      // loads normal angle
+      _sourceAudio.Play();
+      _ballController.PerfectAngleReflect(collision);
+    }
+
+  }
 }
